a82a750a44b2bb870a520f56f682c1d8f7894fb8
zheiii <mohammadnezhadhanieh@gmail.com>
2025-01-31 09:51:23 -0800

    2nd part

--- firstexercise.js
+++ firstexercise.js
@@ -7,28 +7,38 @@ function registerTeam(teamName){
 };
 
 function recordMatchResult(homeTeamName, awayTeamName, homeGoals, awayGoals){
-  team[homeTeamName].goalsScored = homeGoals ;
-  team[homeTeamName].goalsConceded = awayGoals ;
+   for ( let team of teams ) {
+    if (team.name == homeTeamName){
 
-  if ( homeGoals > awayGoals ) {
-    teams[homeTeamName].wins += 1 ;
-  } else if (awayGoals > homeGoals) {
-    teams[awayTeamName].wins += 1 ;
-  } else {
-    teams[homeTeamName].draws += 1
-    teams[awayTeamName].draws += 1
-  }
-}
+      team.goalsScored = homeGoals ;
+      team.goalsConceded = awayGoals ;
 
-// TODO - Create the function here, get the teamName as argument and initialise the remaining properties to default values as shown in the output example below.
+      if ( homeGoals > awayGoals ) {
+        team.wins += 1 ;
+      } else if ( homeGoals < awayGoals ) {
+        team.losses += 1 ;
+      } else {
+        team.draws += 1 ;
+      };
 
-// Example usage:
-registerTeam("Dream FC");
-registerTeam("Phoenix United");
-recordMatchResult("Dream FC", "Phoenix United", 3, 1);
+    } else if (team.name == awayTeamName) {
 
-console.log(teams);
-// [
-//   { name: 'Dream FC', wins: 0, draws: 0, losses: 0, goalsScored: 0, goalsConceded: 0 },
-//   { name: 'Phoenix United', wins: 0, draws: 0, losses: 0, goalsScored: 0, goalsConceded: 0 }
-// ]
+      team.goalsScored = awayGoals ;
+      team.goalsConceded = homeGoals ;
+
+      if ( homeGoals < awayGoals ) {
+        team.wins += 1 ;
+      } else if ( homeGoals > awayGoals ) {
+        team.losses += 1 ;
+      } else {
+        team.draws += 1 ;
+      };
+      
+    };
+   };
+};
+
+// registerTeam("Dream FC");
+// registerTeam("Phoenix United");
+// recordMatchResult("Dream FC", "Phoenix United", 3, 1);
+// console.log(teams);
